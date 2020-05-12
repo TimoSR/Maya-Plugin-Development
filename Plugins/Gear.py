db6b39162ae5f6b5f01be188ad2434a569ef8a48
Unknown <s144146@student.dtu.dk>
2020-05-12 14:33:09 +0200

    Gear Modeler done

--- Plugins/Gear.py
+++ Plugins/Gear.py
@@ -1,7 +1,23 @@
 import maya.cmds as cmds
 
+"""
+# Using: Maya API 1.0 
+# Author: Timothy Stoltzner Rasmussen
+"""
+
 
 class Gear(object):
+
+    standard_length = None
+    standard_teeth = None
+
+    """
+    TestScript:
+    gearA = Gear()
+    gearA.create()
+    gearA.change_teeth(30, 1)
+    gearA.change_length(2)
+    """
 
     def __init__(self, standard_teeth=10, standard_length=0.3):
         """
@@ -13,19 +29,23 @@ class Gear(object):
         """
         self.standard_teeth = standard_teeth
         self.standard_length = standard_length
-        self.spans = standard_length * 2
+        self.spans = standard_teeth * 2
         self.shape = None
         self.transform = None
         self.constructor = None
         self.extrude = None
 
     def create(self):
-        pass
+
+        self.create_pipe()
+
+        self.make_teeth()
 
     def create_pipe(self):
         # Setting the shape and transform to the class variables
         self.transform, self.shape = cmds.polyPipe(subdivisionsAxis=self.spans)
 
+        # Finding the polyPipe node and set it equal to the constructor
         for node in cmds.listConnections("%s.inMesh" % self.transform):
             if cmds.objectType(node) == "polyPipe":
                 self.constructor = node
@@ -35,15 +55,38 @@ class Gear(object):
         # Clearing the selection to ensure, selection is clean.
         cmds.select(clear=True)
 
-        faces = self.get_teeth_faces()
-
-    def change_length(self):
-        cmds.polyExtrudeFacet(self.extrude, edit=True, ltz=self.standard_length)
-
-    def get_teeth_faces(self):
         # Creating a list to select all the faces that will become the teeth.
         # This will return a list of numbers in the range spans * 2 to spans * 3, with steps of 2.
-        side_faces = range(self.spans * 2, self.spans * 3, 2)
+        side_faces = self.get_teeth_faces(self.standard_teeth)
+
+        # Looping through the faces in the list of side_faces
+        for face in side_faces:
+            # The '%s.f[%s]' expands to something like pPipe1.f[20]
+            # ADD selecting the wanted faces on the created polygonPipe.
+            cmds.select("%s.%s" % (self.transform, face), add=True)
+
+        # Instead of returning a value, the extrude note will be stored onto the class
+        # as a class variable
+        self.extrude = cmds.polyExtrudeFacet(localTranslateZ=self.standard_length)[0]
+        cmds.select(clear=True)
+
+    def change_length(self, length=standard_length):
+        # Because the extrude node is on the class, I can get it directly
+        # By doing it like this, Maya don't need to tell what extrude note to change.
+        cmds.polyExtrudeFacet(self.extrude, edit=True, ltz=length)
+
+    def change_teeth(self, teeth=standard_teeth, length=standard_length):
+        # By knowing what constructor is used within maya, I refer to it directly
+        cmds.polyPipe(self.constructor, edit=True, sa=teeth * 2)
+        # Calling modify_extrude directly
+        self.modify_extrude(teeth, length)
+
+    def get_teeth_faces(self, teeth=standard_teeth):
+
+        spans = teeth * 2
+        # Creating a list to select all the faces that will become the teeth.
+        # This will return a list of numbers in the range spans * 2 to spans * 3, with steps of 2.
+        side_faces = range(spans * 2, spans * 3, 2)
 
         # The list for collecting the faces
         faces = []
@@ -54,9 +97,8 @@ class Gear(object):
 
         return faces
 
-
-    def modify_extrude(self):
-        faces = self.get_teeth_faces()
+    def modify_extrude(self, teeth=standard_teeth, length=standard_length):
+        faces = self.get_teeth_faces(teeth)
 
         # The extrude node has an attribute called inputComponents
         # To change it I use a simple SetAttr call instead of recreating the extrude which can be expensive
@@ -66,4 +108,7 @@ class Gear(object):
 
         cmds.setAttr("%s.inputComponents" % self.extrude, len(faces), *faces, type="componentList")
 
-        self.change_length()
+        self.change_length(length)
+
+
+
---
+++ Plugins/GearScript.py
@@ -0,0 +1,83 @@
+import maya.cmds as cmds
+
+
+# Using the Maya 1.0 APIs
+
+def gear_creator(standard_teeth=10, standard_length=0.3):
+    """
+    This function will create a gear with the given parameters
+    Args:
+        standard_teeth: The number of teeth to create
+        standard_length: The length of the teeth
+    Returns:
+        A tuple of the transform, constructor and extrude node
+    """
+
+    # Teeth are every second face, so doubling the number of teeth to get the number of spans required.
+    spans = standard_teeth * 2
+
+    # The polyPipe command will create a polygon pipe the subdivisionsAxis will say how many divisions it will have
+    # along it's length. It returns a list of [transform, constructor]. Instead of getting a list and then extracting
+    # it's members, it is directly expanded it to the variables. The transform is the name of the node created
+    # and the constructor is the node that creates the pipe and controls its parameters
+    transform, constructor = cmds.polyPipe(subdivisionsAxis=spans)
+
+    # Creating a list to select all the faces that will become the teeth.
+    # This will return a list of numbers in the range 40 to 60, with steps of 2.
+    side_faces = range(spans * 2, spans * 3, 2)
+
+    print "Selecting the faces %s" % side_faces
+
+    # Clearing the selection, to ensure only faces is added.
+    cmds.select(clear=True)
+
+    # Looping through the faces in the list of side_faces
+    for face in side_faces:
+        # The '%s.f[%s]' expands to something like pPipe1.f[20]
+        # ADD selecting the wanted faces on the created polygonPipe.
+        cmds.select("%s.f[%s]" % (transform, face), add=True)
+
+    # Extruding the selected faces by the given length
+    # This gives back the value of the extrude node inside a list
+    extrude = cmds.polyExtrudeFacet(localTranslateZ=standard_length)[0]
+
+    print extrude
+
+    # Returning a tuple of (transform, constructor, extrude). A tuple is similar to a list but cannot be modified.
+    # The transform is our gear node, the constructor is the node that creates the original pipe and
+    # the extrude is the node that extrudes the faces
+    return transform, constructor, extrude
+
+
+def gear_teeth_modifier(constructor, extrude, standard_teeth=10, length=0.3):
+    """
+    Change the number of teeth on a gear with a given number of teeth and a given length for the teeth.
+    This will create a new extrude node.
+
+    Args:
+        constructor (str): the constructor node
+        extrude (str): the extrude node
+        teeth (int): the number of teeth to create
+        length (float): the length of the teeth tp create
+    """
+
+    spans = standard_teeth * 2;
+
+    # Modifying its attributes instead of creating a new one
+    cmds.polyPipe(constructor, edit=True, subdivisionsAxis=spans)
+
+    # List of faces to extrude as teeth
+    side_faces = range(spans * 2, spans * 3, 2)
+    face_names = []
+
+    # Collecting the face names
+    for face in side_faces:
+        face_name = "f[%s]" % face
+        face_names.append(face_name)
+
+    # cmds.setAttr('extrudeNode.inputComponents', numberOfItems, item1, item2, item3, type='componentList')
+    # Example cmds.setAttr('extrudeNode.inputComponents', 2, 'f[1]', 'f[2]', type='componentList'
+
+    cmds.setAttr("%s.inputComponents" % extrude, len(face_names), *face_names, type="componentList")
+
+    cmds.polyExtrudeFacet(extrude, edit=True, ltz=length)
