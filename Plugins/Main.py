145313916728e7557db7f59703469a58e80e4e81
Unknown <s144146@student.dtu.dk>
2020-05-13 14:39:07 +0200

    Update project

--- Plugins/Main.py
+++ Plugins/Main.py
@@ -10,6 +10,8 @@ Requirements: A simple animation in maya, to showcase timeline editing.
 
 class ReTimer(object):
 
+    # Method Types Python
+    # https://levelup.gitconnected.com/method-types-in-python-2c95d46281cd
     # What is @classmethod
     # https://www.programiz.com/python-programming/methods/built-in/classmethod
 
@@ -66,6 +68,31 @@ class ReTimer(object):
         # Time is the same time because its targeting the same frame.
         # TimeChange is the time we want to change to.
         cmds.keyframe(edit=True, time=(current_time, current_time), timeChange=new_time)
+
+    @classmethod
+    def get_start_keyframe_time(cls, range_start_time):
+        """
+        Get the start keyframe time or the previous.
+        :param range_start_time:
+        :return: The current chosen or previous keyframe if no keyframe exist.
+        """
+        # Maya keyframe command.
+        start_times = cmds.keyframe(query=True, time=(range_start_time, range_start_time))
+        # If there is a list returned with any values.
+        if start_times:
+            # I return the keyframe.
+            return start_times[0]
+        # If no keyframes exists I return the previous keyframe.
+        # Using the find_keyframe method.
+        start_time = cls.find_keyframe("previous", range_start_time)
+        return start_time
+
+    @classmethod
+    def get_last_keyframe_time(cls):
+        """
+        :return: The last Keyframe time.
+        """
+        return cls.find_keyframe("last")
 
 
 if __name__ == "__main__":
